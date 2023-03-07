884c03286d8b45b6335a9ceb55aa8a40e111a189
ramiqayya <rami.qayya@gmail.com>
2023-03-07 13:13:34 +0200

    test some stuff

--- final/test/views.py
+++ final/test/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return render(request, "test/index.html")
+
+
+def greet(request, name):
+    return render(request, "test/greet.html", {
+        "name": name.title()
+
+    })
